f305c98720ed75a4059a72369250585bbd45221e
Amanda <Amanda@AmandaPC>
2017-10-10 12:10:16 -0500

    added bootstrap. deleted classes.py. added welcome page and CRUD system

--- WebContent/app.py
+++ WebContent/app.py
@@ -1,11 +1,44 @@
+# import the Flask class from the flask module
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-import classes as c
 
+
+# create the application object
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Spitfire:12Reddoor34@tms-capstone.cglah7gng54k.us-west-2.rds.amazonaws.com/CapstoneDB'
 db = SQLAlchemy(app)
+app.secret_key = "super secret key"
+
+class users(db.Model):
+    __tablename__ = "users"
+    idUsers = db.Column('idUsers', db.Integer, primary_key=True)
+    User = db.Column('User', db.String(45))
+    Password = db.Column('Password', db.String(45))
+
+    def __init__(self, idUsers, User, Password):
+        self.idUsers = idUsers
+        self.User = User
+        self.Password = Password
+
+    def __repr__(self):
+        return '<User %r>' % self.User
+    
+class item(db.Model):
+    __tablename__ = "items"
+    idItems = db.Column('idItems', db.Integer, primary_key=True)
+    Name = db.Column('Name', db.String(45))
+    Quantity = db.Column('Quantity', db.Integer)
+    Barcode = db.Column('Barcode', db.String(45))
+    
+    def __init__(self, idItems, Name, Quantity, Barcode):
+        self.idItems = idItems
+        self.Name = Name
+        self.Quantity = Quantity
+        self.Barcode = Barcode
+        
+    def __repr__(self):
+        return '<Item %r>' % self.Name 
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -20,52 +53,33 @@ def welcome():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method == 'POST':
-		holder = c.users.query.get(request.form['username'])
-		if holder != None:
-			if holder.Password != request.form['password']:
-				error = 'Invalid Credentials. Please try again.'
-			else:
-				return redirect(url_for('database'))
-		else:
-			error = 'Invalid Credentials. Please try again.'
-    return render_template('login.html', error=error)
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = users.query.filter_by(User=username,Password=password).first()
+    if  registered_user is None:
+        error = 'Invalid Credentials. Please try again.'
+        return render_template('login.html', error=error)
+    return redirect(url_for('welcome'))
 
 @app.route('/database')
 def database():
-	if request.method == 'POST':
-		if request.form['query'] == add:
-			return redirect(url_for('add'))
-		if request.form['query'] == update:
-			return redirect(url_for('update'))
-		if request.form['query'] == delete:
-			return redirect(url_for('delete))
-		if request.form['query'] == read:
-			return redirect(url_for('read')
-			
+    items = item.query.all()
+    return render_template('database.html', items=items)  # render a template
+
 @app.route('/add')
 def add():
-	if request.method == 'POST':
-		item = c.items(request.form['idItems'], request.form['Name'], request.form['Quantity'], request.form['Barcode']
-		c.db.session.add(item)
-		c.db.session.commit()
-		
+    return render_template('add.html')  # render a template
+
 @app.route('/update')
-def update():
-	
-	
+def update(idItems):
+    return render_template('update.html')  # render a template
+
 @app.route('/delete')
-def delete():
-	if request.method == 'POST':
-		c.items.query.filter_by(idItems=request.form['id']).delete()
-		c.db.session.commit()
-	
-@app.route('/read')
-def read():
-
-
+def delete(idItems):
+    return render_template('delete.html')  # render a template
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
-	
